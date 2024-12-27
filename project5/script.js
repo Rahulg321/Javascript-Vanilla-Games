c77d848ea061006e5e1b5d4aa2c78dc1db0ce74e
Rahul Gupta <rg5353070@gmail.com>
2024-12-27 06:13:14 +0530

    added explosions when a raven is clicked

--- project5/script.js
+++ project5/script.js
@@ -5,9 +5,17 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+/**  @type {HTMLCanvasElement} */
+const collisionCanvas = document.getElementById("collisionCanvas");
+const collisionCtx = collisionCanvas.getContext("2d");
+
+collisionCanvas.width = window.innerWidth;
+collisionCanvas.height = window.innerHeight;
+
 ctx.font = "50px Impact";
 
 let score = 0;
+let gameOver = false;
 let ravens = [];
 
 let timeToNextRaven = 0;
@@ -38,10 +46,19 @@ class Raven {
     this.maxFrame = 4;
 
     this.timeSinceFlap = 0;
+
+    // random value between 50 and 100
     this.flapInterval = Math.random() * 50 + 50;
+    this.randomColors = [
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),
+    ];
+    this.color = `rgb(${this.randomColors[0]},${this.randomColors[1]},${this.randomColors[2]})`;
   }
 
   update(deltaTime) {
+    // if it touches the top of the screen
     if (this.y < 0 || this.y > canvas.height - this.height) {
       this.directionY = -this.directionY;
     }
@@ -59,9 +76,14 @@ class Raven {
 
       this.timeSinceFlap = 0;
     }
+
+    // if any raven crosses over without being clicked
+    if (this.x < 0 - this.width) gameOver = true;
   }
 
   draw() {
+    collisionCtx.fillStyle = this.color;
+    collisionCtx.fillRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(
       this.image,
       this.frame * this.spriteWidth,
@@ -76,6 +98,51 @@ class Raven {
   }
 }
 
+let explosions = [];
+
+class Explosion {
+  constructor(x, y, size) {
+    this.image = new Image();
+    this.image.src = "boom.png";
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.size = size;
+    this.x = x;
+    this.y = y;
+    this.frame = 0;
+    this.sound = new Audio();
+    this.sound.src = "boom.wav";
+    this.timeSinceLastFrame = 0;
+    this.frameInterval = 200;
+    this.markedForDeletion = false;
+  }
+
+  update(deltaTime) {
+    if (this.frame === 0) this.sound.play();
+
+    this.timeSinceLastFrame += deltaTime;
+    if (this.timeSinceLastFrame > this.frameInterval) {
+      this.frame++;
+      this.timeSinceLastFrame = 0;
+      if (this.frame > 5) this.markedForDeletion = true;
+    }
+  }
+
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y - this.size / 4,
+      this.size,
+      this.size
+    );
+  }
+}
+
 function drawScore() {
   ctx.fillStyle = "black";
   ctx.fillText(`Score:- ${score}`, 50, 75);
@@ -83,8 +150,46 @@ function drawScore() {
   ctx.fillText(`Score:- ${score}`, 55, 80);
 }
 
+function drawGameOver() {
+  ctx.textAlign = "center";
+  ctx.fillStyle = "black";
+  ctx.fillText(
+    `GAME OVER, your score is ${score}`,
+    canvas.width / 2,
+    canvas.height / 2
+  );
+  ctx.fillStyle = "white";
+  ctx.fillText(
+    `GAME OVER, your score is ${score}`,
+    canvas.width / 2 + 5,
+    canvas.height / 2 + 5
+  );
+}
+
+window.addEventListener("click", (object) => {
+  // we are scanning just one pixel color
+  const detectPixelColor = collisionCtx.getImageData(object.x, object.y, 1, 1);
+  console.log(detectPixelColor);
+  const pc = detectPixelColor.data;
+  console.log(pc);
+  ravens.forEach((e) => {
+    if (
+      e.randomColors[0] === pc[0] &&
+      e.randomColors[1] === pc[1] &&
+      e.randomColors[2] === pc[2]
+    ) {
+      // detects collision by color
+      e.markedForDeletion = true;
+      score++;
+
+      explosions.push(new Explosion(e.x, e.y, e.width));
+    }
+  });
+});
+
 function animate(timestamp) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  collisionCtx.clearRect(0, 0, canvas.width, canvas.height);
   //   time passed between frames
   let deltaTime = timestamp - lastTime;
   lastTime = timestamp;
@@ -93,15 +198,22 @@ function animate(timestamp) {
   if (timeToNextRaven > ravenInterval) {
     ravens.push(new Raven());
     timeToNextRaven = 0;
+
+    // large ravens are on top and smaller ravens are on the bottom
+    ravens.sort((a, b) => {
+      return a.width - b.width;
+    });
   }
 
   drawScore();
-  [...ravens].forEach((e) => e.update(deltaTime));
-  [...ravens].forEach((e) => e.draw());
+  [...ravens, ...explosions].forEach((e) => e.update(deltaTime));
+  [...ravens, ...explosions].forEach((e) => e.draw());
 
-  ravens.filter((e) => !e.markedForDeletion);
+  ravens = ravens.filter((e) => !e.markedForDeletion);
+  explosions = explosions.filter((e) => !e.markedForDeletion);
 
-  requestAnimationFrame(animate);
+  if (!gameOver) requestAnimationFrame(animate);
+  else drawGameOver();
 }
 
 animate(0);
